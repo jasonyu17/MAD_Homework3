6784a7217709f9c2588d1da7bcb02557882b5a1d
jasonyu17 <jason.yu171@gmail.com>
2025-03-14 16:38:12 -0400

    created skeleton for card game app

---
+++ lib/models/cards.dart
@@ -0,0 +1,14 @@
+class Cards {
+  final int cardNumber;
+  final String cardSuite;
+  final int cardID;
+  final String cardImagePath;
+  
+
+  Cards({
+    required this.cardNumber,
+    required this.cardSuite,
+    required this.cardID,
+    required this.cardImagePath,
+  });
+}
